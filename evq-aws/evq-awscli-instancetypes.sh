258d45d0da2e2d0a43479384687ff69ddae04004
Isaiah Narisma <narisma.isaiah@gmail.com>
2022-02-16 16:51:28 -0800

    Addition of AWS CLI scripts

---
+++ evq-aws/evq-awscli-instancetypes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# aws configure
+# AWS Access Key ID [****************72UR]:
+# AWS Secret Access Key [****************D+iG]:
+# Default region name [us-west-1]:
+# Default output format [json]:
+
+aws ec2 describe-instances --region="us-west-1" |
+jq -r '.Reservations[].Instances[]|.InstanceId+" "+.InstanceType+" "+(.Tags[] | select(.Key == "Name").Value)'
+
+#list roles
+
+aws iam list-instance-profiles-for-role
+--role-name ec2Admin |
+jq -r
+
+done
