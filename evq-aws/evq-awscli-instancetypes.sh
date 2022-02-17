20850dd1d74cd4276c43d97dd1d36776cdc5f178
Isaiah Narisma <narisma.isaiah@gmail.com>
2022-02-17 09:37:28 -0800

    Update in code

--- evq-aws/evq-awscli-instancetypes.sh
+++ evq-aws/evq-awscli-instancetypes.sh
@@ -6,13 +6,11 @@
 # Default region name [us-west-1]:
 # Default output format [json]:
 
-aws ec2 describe-instances --region="us-west-1" |
-jq -r '.Reservations[].Instances[]|.InstanceId+" "+.InstanceType+" "+(.Tags[] | select(.Key == "Name").Value)'
+aws ec2 describe-instances --region="us-west-1" | jq -r '.Reservations[].Instances[]|.InstanceId+" "+.InstanceType+" "+(.Tags[] | select(.Key == "Name").Value)'
 
 #list roles
 
 aws iam list-instance-profiles-for-role
---role-name ec2Admin |
-jq -r
+--role-name ec2Admin | jq -r
 
 done
